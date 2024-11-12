871b177b1886c647a2864851cb16f63ba9aa8f86
Diego <diego.scavalcanti@gmail.com>
2024-11-11 22:04:21 -0300

    feat: MainActivity, LoginFragment e SignUpFragment

---
+++ app/src/main/java/com/example/mavidenergyapp/fragments/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.example.mavidenergyapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mavidenergyapp.R
+import com.example.mavidenergyapp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!  // Safe property access
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/mavidenergyapp/fragments/SignUpFragment.kt
@@ -0,0 +1,28 @@
+package com.example.mavidenergyapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mavidenergyapp.R
+import com.example.mavidenergyapp.databinding.FragmentSignUpBinding
+
+class SignUpFragment : Fragment() {
+
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!  // Safe property access
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
